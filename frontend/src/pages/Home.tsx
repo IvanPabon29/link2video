3e9cddeb49f968e12ce230cb26bebf484d573b8e
Iván Pabón <176315757+IvanPabon29@users.noreply.github.com>
2025-10-08 18:17:03 -0500

    Crear pages Home y NotFound

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+
+function Home() {
+  return (
+    <div className="text-center">
+      <h1>Bienvenido a Link2Video</h1>
+      <p>Ingresa un enlace y descarga tu video en diferentes formatos y resoluciones.</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ frontend/src/pages/NotFound.tsx
@@ -0,0 +1,11 @@
+
+function NotFound() {
+  return (
+    <div className="text-center">
+      <h1>404</h1>
+      <p>Página no encontrada</p>
+    </div>
+  );
+}
+
+export default NotFound;
