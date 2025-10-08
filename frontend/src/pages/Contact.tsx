b40c167ae65f01ab5ffcb8b46971d36762818fd0
Iván Pabón <176315757+IvanPabon29@users.noreply.github.com>
2025-10-08 18:16:32 -0500

    Crear pages Contact y Features

---
+++ frontend/src/pages/Contact.tsx
@@ -0,0 +1,11 @@
+
+function Contact() {
+  return (
+    <div className="container">
+      <h1>Contacto</h1>
+      <p>Si tienes dudas, escríbenos a: soporte@link2video.com</p>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ frontend/src/pages/Features.tsx
@@ -0,0 +1,15 @@
+
+function Features() {
+  return (
+    <div className="container">
+      <h1>Funcionalidades</h1>
+      <ul>
+        <li>Descargar videos en múltiples resoluciones</li>
+        <li>Soporte para YouTube, Facebook y más</li>
+        <li>Conversión a diferentes formatos</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Features;
