6743d8702d1144a71741c31fb7ffa9b44e53c9ab
Iván Pabón <176315757+IvanPabon29@users.noreply.github.com>
2025-10-16 17:12:59 -0500

    Importar dependecias necesarias

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+// Bootstrap (desde npm)
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-// Importar Bootstrap (CSS + JS)
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// Iconos de Bootstrap
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+// Estilos globales
+import "./styles/global.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
