cc677586063056f873c113c2d9d768ba7b2faa76
Iván Pabón <176315757+IvanPabon29@users.noreply.github.com>
2025-11-21 19:33:50 -0500

    refactor(VideoInfoCard): actualizar autor de los videos y limpiar el código

--- frontend/src/components/VideoInfoCard.tsx
+++ frontend/src/components/VideoInfoCard.tsx
@@ -11,17 +11,19 @@ import "./styles/VideoInfoCard.css";
 interface Props {
   thumbnail: string;
   title: string;
+  uploader: string;
   duration: string;
 }
 
-const VideoInfoCard = ({ thumbnail, title, duration }: Props) => {
+const VideoInfoCard = ({ thumbnail, title, uploader, duration }: Props) => {
   return (
     <div className="video-info-card">
       <img src={thumbnail} alt={title} className="video-info-thumb" />
 
       <div className="video-info-details">
-        <h3 className="video-info-title">{title}</h3>
-        <span className="video-info-duration">{duration}</span>
+        <h3 className="video-info-title">Titulo: {title}</h3>
+        <span className="video-info-author">Autor: {uploader}</span>
+        <span className="video-info-duration">Duración: {duration}</span>
       </div>
     </div>
   );
